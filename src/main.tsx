a934b0a60f7613e01e218c94eb444a3f49596403
tinhbanlunglinh-pixel <tinhbanlunglinh@gmail.com>
2026-05-27 06:02:55 +0700

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Polyfill process for libraries that expect it (like some AI SDKs)
+if (typeof window !== 'undefined') {
+  window.process = window.process || { env: {} } as any;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
